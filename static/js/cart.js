6ac7a0cb3ff7108dd6a92cf6448d70f5a77f0eb7
RantoSarobidy <mirantosarobidy07@gmail.com>
2024-03-03 22:36:22 +0300

    Ajout fichier cart.js

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,1 +1,17 @@
-console.log("Hello you!!")
+//console.log("Hello you!!")
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for (i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function(){
+        var productId = this.dataset.productid
+        var action = this.dataset.action
+        console.log('productId:', productId, 'Action:', action)
+
+        console.log('USER:', user)
+        if (user == 'AnonymousUser') {
+            console.log('User is not authenticated')
+        } else {
+            console.log('User is authenticated, sending data...')
+        }
+    })
+}
